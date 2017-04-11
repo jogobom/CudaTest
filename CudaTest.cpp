85f7c159252bbb9a6c324fe4c6e8d33487960bf8
Chris Preston <chris_preston@waters.com>
2017-04-11 14:07:43 +0100

    CUDA now initialised

--- CudaTest.cpp
+++ CudaTest.cpp
@@ -1,6 +1,8 @@
 #pragma warning(push,0)
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <cuda_runtime.h>
+#include <helper_cuda.h>
 #pragma warning(pop)
 
 TEST(ItFails, ExampleTests)
@@ -10,6 +12,9 @@ TEST(ItFails, ExampleTests)
 
 int main(int ac, char* av[])
 {
+	checkCudaErrors(cudaSetDevice(0));
+	
 	testing::InitGoogleTest(&ac, av);
+
 	return RUN_ALL_TESTS();
 }
