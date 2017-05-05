66a78ea1593742c36755af036fa1f44643e3d97c
Chris Preston <chris_preston@waters.com>
2017-05-05 13:43:33 +0100

    First working test
    CPU and GPU implementation of identity compared in a simple test.

--- CudaTest.cpp
+++ CudaTest.cpp
@@ -5,9 +5,58 @@
 #include <helper_cuda.h>
 #pragma warning(pop)
 
-TEST(ItFails, ExampleTests)
+template <typename T>
+T* AllocOnDevice(size_t count)
 {
-	FAIL();
+	T* d_buffer;
+	checkCudaErrors(cudaMalloc(&d_buffer, count*sizeof(T)));
+	return d_buffer;
+}
+
+template <typename T>
+T* CopyToDevice(std::vector<T> const& vec)
+{
+	T* d_buffer = AllocOnDevice<T>(vec.size());
+	checkCudaErrors(cudaMemcpy(d_buffer, &vec[0], vec.size() * sizeof(T), cudaMemcpyHostToDevice));
+	return d_buffer;
+}
+
+template <typename T>
+std::vector<T> CopyFromDevice(T const* data, size_t const length)
+{
+	std::vector<T> result(length, 42.f);
+	checkCudaErrors(cudaMemcpy(&result[0], data, length * sizeof(T), cudaMemcpyDeviceToHost));
+	return result;
+}
+
+std::vector<float> CpuVersion(std::vector<float> const& input)
+{
+	return input;
+}
+
+void kernelWrapper(float const* const input, float* const output, const int rows, const int cols);
+
+TEST(ExampleTests, Identity)
+{
+	auto rows = 2;
+	auto cols = 1;
+	auto length = rows * cols;
+
+	std::vector<float> input;
+	for (auto i = 0; i < rows; ++i)
+		for (auto j = 0; j < cols; ++j)
+			input.push_back(static_cast<float>(j + i*cols));
+
+	const auto d_input = CopyToDevice<float>(input);
+	const auto d_output = AllocOnDevice<float>(length);
+
+	kernelWrapper(d_input, d_output, rows, cols);
+
+	const auto gpuOutput = CopyFromDevice(d_output, length);
+
+	const auto cpuOutput = CpuVersion(input);
+
+	ASSERT_THAT(gpuOutput, testing::ContainerEq(cpuOutput));
 }
 
 int main(int ac, char* av[])
